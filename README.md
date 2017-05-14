c9116c603451bae5ee53e6848cf9f1fde548dc16
taroxd <taroxd@outlook.com>
2017-05-14 10:40:58 +0800

    allow rtp option

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Or with more options:
 ```ruby
 require 'rgss3'
 RGSS3::RTP.path = 'path/to/RTP'
-RGSS3.run(width: 544, height: 416, frame_rate: 60, title: "Game") do
+RGSS3.run(width: 544, height: 416, frame_rate: 60, title: 'Game', rtp: 'path/to/rtp') do
   # TODO: Add your code here
   # loop { Graphics.update }
 end
--- lib/rgss3/game_window.rb
+++ lib/rgss3/game_window.rb
@@ -3,8 +3,9 @@ require 'fiber'
 module RGSS3
   class GameWindow < Gosu::Window
     attr_reader :frame_rate
-    def initialize(width: 544, height: 416, full_screen: false, frame_rate: 60, title: "Game")
+    def initialize(width: 544, height: 416, full_screen: false, frame_rate: 60, title: "Game", rtp: nil)
       @frame_rate = frame_rate
+      RTP.path = rtp if rtp
       super(width, height, full_screen, 1000.0 / frame_rate)
       self.caption = title
     end
